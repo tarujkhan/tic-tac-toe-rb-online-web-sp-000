9b6e6aeb73502bd010c683b9948e5d762bfa09ff
taroj khan <tarujkhan@yahoo.com>
2020-09-01 17:15:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -188,7 +188,7 @@ if won?(board)
   puts "Congratulations #{current_player(board)}!"
 else 
   if draw?(board)
-  puts prints "Cat\'s Game!" on a draw."
+  puts prints "Cat\'s Game!"
 end
 end
 end
