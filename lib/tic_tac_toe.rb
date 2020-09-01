abceabd8fc304106ba89b3000c64b188da5dfe10
taroj khan <tarujkhan@yahoo.com>
2020-09-01 18:11:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -185,7 +185,7 @@ def play(board)
 #end
 
 if won?(board)
-  puts "Congratulations #{current_player(board)}!"
+  puts "Congratulations #{winner(board)}!"
 elsif draw?(board)
   puts "Cat\'s Game!"
 end
