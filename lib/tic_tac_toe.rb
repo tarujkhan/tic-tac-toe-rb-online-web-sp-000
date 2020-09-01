c33ca6ebf1b79e8d22ac710cb20a293355f45516
taroj khan <tarujkhan@yahoo.com>
2020-09-01 18:30:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,7 +17,7 @@ def position_taken?(board, index)
   #   false 
   # elsif board[index] == nil 
   #   false 
-  elsif board[index] == "X" || board[index] == "O"
+  board[index] == "X" || board[index] == "O"
 #     true
 # end
 end
