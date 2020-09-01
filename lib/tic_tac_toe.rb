c9509f7acc4e93903d0eae5f728e50f7bea25d3c
taroj khan <tarujkhan@yahoo.com>
2020-09-01 18:22:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,6 +43,9 @@ def input_to_index(user_input)
 end
 
 def turn(board)
+  puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
   while !valid_move?(board, index)
     puts "Please enter 1-9:"
     user_input = gets.strip
