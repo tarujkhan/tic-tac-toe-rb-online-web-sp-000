5e1da3691a782521f0c84c0b58b787a3867952d4
taroj khan <tarujkhan@yahoo.com>
2020-09-01 17:35:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,7 +47,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-   move(board, index, token = "X")
+   move(board, index)
     display_board(board)
   else 
     #turn(board)
