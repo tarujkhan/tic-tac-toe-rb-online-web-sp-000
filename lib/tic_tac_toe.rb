3242cad7397678c19b6738c9b21b864db2e3d77e
taroj khan <tarujkhan@yahoo.com>
2020-09-01 18:33:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,17 +10,18 @@ puts " #{board[6]} #{divider} #{board[7]} #{divider} #{board[8]} "
 end
 
 def position_taken?(board, index)
-  puts display_board(board)
-  if board[index] == " "
-    false
-  elsif board[index] == ""
-    false 
-  elsif board[index] == nil 
-    false 
-  elsif board[index] == "X" || board[index] == "O"
-    true
+  # puts display_board(board)
+  # if board[index] == " "
+  #   false
+  # elsif board[index] == ""
+  #   false 
+  # elsif board[index] == nil 
+  #   false 
+ # elsif 
+  board[index] == "X" || board[index] == "O"
+   # true
 end
-end
+
 
 def valid_move?(board, index)
   #binding.pry
